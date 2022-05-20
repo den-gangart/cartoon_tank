212de9e681020b0eee68f02fb9a4874d3f75ee68
Denis-Mozgovyy <denismozgovoj07@gmail.com>
2022-05-20 17:02:13 +0300

    Added shoot effects

--- Assets/Scripts/Tank/GunShooter.cs
+++ Assets/Scripts/Tank/GunShooter.cs
@@ -6,7 +6,13 @@ public class GunShooter : MonoBehaviour
 {
     [SerializeField] private GameObject _shootPoint;
     [SerializeField] private GameObject _bulletPrefab;
+
+    [SerializeField] private GameObject _fxPoint;
+    [SerializeField] private GameObject _shootFX;
+
     [SerializeField] private float _reoladTime = 1f;
+
+    [SerializeField] private Animator _animator;
 
     private bool _isReady = true;
 
@@ -17,14 +23,25 @@ public class GunShooter : MonoBehaviour
             return;
         }
 
-       Instantiate(_bulletPrefab, _shootPoint.transform.position, transform.rotation);
-        _isReady = false;
+        MakeShoot();
         StartCoroutine(Reolad());
+        _isReady = false;
     }
 
     private IEnumerator Reolad()
     {
         yield return new WaitForSeconds(_reoladTime);
         _isReady = true;
+    }
+
+    private void MakeShoot()
+    {
+        Instantiate(_bulletPrefab, _shootPoint.transform.position, transform.rotation);
+        Instantiate(_shootFX, _fxPoint.transform.position, transform.rotation);
+
+        if (_animator != null)
+        {
+            _animator.SetTrigger("Shoot");
+        }
     }
 }
